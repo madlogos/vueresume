0e560d870e11683d1e416f2c600ebe04d4e42b24
Yiying Wang <madlogos@gmail.com>
2022-01-06 22:06:59 +0800

    20220106

--- vue.config.js
+++ vue.config.js
@@ -2,32 +2,32 @@ const path = require('path')
 const cdn = {
   dev: {
     css: [
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/solid.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/brands.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/svg-with-js.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/element-ui/2.13.0/theme-chalk/index.css',
-      'https://unpkg.com/element-ui@2.13.0/lib/theme-chalk/display.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/solid.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/brands.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/svg-with-js.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.7/theme-chalk/index.css',
+      'https://unpkg.com/element-ui@2.15.7/lib/theme-chalk/display.css',
     ],
     js: []
   },
   prod: {
     css: [
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/solid.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/brands.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.13.0/css/svg-with-js.min.css',
-      'https://cdn.bootcdn.net/ajax/libs/element-ui/2.13.2/theme-chalk/index.css',
-      'https://unpkg.com/element-ui@2.13.2/lib/theme-chalk/display.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/solid.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/brands.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/svg-with-js.min.css',
+      'https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.7/theme-chalk/index.css',
+      'https://unpkg.com/element-ui@2.15.7/lib/theme-chalk/display.css',
     ],
     js: [
-      'https://cdn.bootcdn.net/ajax/libs/vue/2.6.11/vue.runtime.min.js',
-      'https://cdn.bootcdn.net/ajax/libs/vue-router/3.2.0/vue-router.min.js',
-      'https://cdn.bootcdn.net/ajax/libs/vuex/3.2.0/vuex.min.js',
-      'https://cdn.bootcdn.net/ajax/libs/echarts/4.8.0/echarts.min.js',
-      'https://cdn.bootcdn.net/ajax/libs/axios/0.19.2/axios.min.js',
-      'https://cdn.bootcdn.net/ajax/libs/vue-i18n/8.18.1/vue-i18n.min.js',
-      'https://cdn.bootcdn.net/ajax/libs/element-ui/2.13.2/index.js',
+      'https://cdn.bootcdn.net/ajax/libs/vue/2.6.14/vue.runtime.min.js',
+      'https://cdn.bootcdn.net/ajax/libs/vue-router/3.5.3/vue-router.min.js',
+      'https://cdn.bootcdn.net/ajax/libs/vuex/3.6.2/vuex.min.js',
+      'https://cdn.bootcdn.net/ajax/libs/echarts/5.2.2/echarts.min.js',
+      'https://cdn.bootcdn.net/ajax/libs/axios/0.24.0/axios.min.js',
+      'https://cdn.bootcdn.net/ajax/libs/vue-i18n/8.26.8/vue-i18n.min.js',
+      'https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.7/index.js',
     ]
   }
 }
